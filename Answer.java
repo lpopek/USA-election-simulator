2af29077305ddad7fb562ff28cf57fb119791457
okrupa <o.krupa@stud.elka.pw.edu.pl>
2020-12-07 23:13:28 +0100

    add debate

---
+++ Answer.java
@@ -0,0 +1,11 @@
+public class Answer {
+    int GOPSupport;
+    int DEMSupport;
+    String answer;
+    public  Answer(int GOPSupport,int DEMSupport, String answer) {
+        this.GOPSupport = GOPSupport;
+        this.DEMSupport = DEMSupport;
+        this.answer = answer;
+    }
+
+}
