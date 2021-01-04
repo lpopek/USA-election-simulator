179b273ea00f25ef60d0ecc1d0e9849cbaed75b4
Łukasz Popek <01123885@pw.edu.pl>
2021-01-04 12:49:50 +0100

    main winodw of program

--- src/main/java/USA_ELECTION_SIMULATOR/App.java
+++ src/main/java/USA_ELECTION_SIMULATOR/App.java
@@ -1,17 +1,24 @@
 package USA_ELECTION_SIMULATOR;
 
-/**
- * Hello world!
- */
-public final class App {
-    private App() {
-    }
+import java.io.File;
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        launch(args);
     }
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(new File("main_view.fxml").toURI().toURL());
+        Parent root = loader.load();
+         Scene scene = new Scene(root);
+         stage.setTitle("USA_Simulator");
+         stage.setScene(scene);
+         stage.show();
+     }
 }
