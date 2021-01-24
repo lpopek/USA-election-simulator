6eb87d4b0c2db104eedb9358341aca95a5e13f90
Łukasz Popek <01123885@pw.edu.pl>
2021-01-24 23:11:42 +0100

    all states without AL, HI, DC

--- src/main/java/USA_ELECTION_SIMULATOR/FMXLDocumentController.java
+++ src/main/java/USA_ELECTION_SIMULATOR/FMXLDocumentController.java
@@ -42,6 +42,10 @@ public class FMXLDocumentController implements Initializable {
     @FXML private Polygon AZ;
     @FXML private Polygon CA;
     @FXML private Polygon CO;
+    @FXML private Polygon CT;
+    @FXML private Polygon DE;
+    @FXML private Polygon FL;
+    @FXML private Polygon GA;
     @FXML private Polygon ID;
     @FXML private Polygon IL;
     @FXML private Polygon IN;
@@ -49,21 +53,37 @@ public class FMXLDocumentController implements Initializable {
     @FXML private Polygon KS;
     @FXML private Polygon KY;
     @FXML private Polygon LA;
+    @FXML private Polygon ME;
+    @FXML private Polygon MD;
+    @FXML private Polygon MA;
+    @FXML private Polygon MI;
     @FXML private Polygon MN;
     @FXML private Polygon MS;
     @FXML private Polygon MO;
     @FXML private Polygon MT;
     @FXML private Polygon NE;
-    @FXML private Polygon NM;
     @FXML private Polygon NV;
+    @FXML private Polygon NH;
+    @FXML private Polygon NJ;
+    @FXML private Polygon NM;
+    @FXML private Polygon NY;
+    @FXML private Polygon NC;
     @FXML private Polygon ND;
+    @FXML private Polygon OH;
     @FXML private Polygon OK;
     @FXML private Polygon OR;
+    @FXML private Polygon PA;
+    @FXML private Polygon RI;
+    @FXML private Polygon SC;
     @FXML private Polygon SD;
     @FXML private Polygon TN;
     @FXML private Polygon TX;
     @FXML private Polygon UT;
+    @FXML private Polygon VA;
+    @FXML private Polygon VT;
     @FXML private Polygon WA;
+    @FXML private Polygon WI;
+    @FXML private Polygon WV;
     @FXML private Polygon WY;
 
     ///////////ALABAMA///////////
@@ -170,9 +190,39 @@ public class FMXLDocumentController implements Initializable {
 
     State ct = App.GAME.USA.get(6);
 
+    @FXML void colorStateCT(){
+        colorState(CT, ct);
+    }
+
+    @FXML void clickStateCT(Event event){
+        CT.setFill(Color.GREEN);
+        showPieChart(ct);
+    }
+    @FXML void hoverOverStateCT(Event event){
+        CT.setFill(Color.LIGHTGREEN);
+    }
+    @FXML void exitOverStateCT(Event event){
+        colorStateCT();
+    }
+
     ///////////DELAWARE///////////
 
     State de = App.GAME.USA.get(7);
+
+    @FXML void colorStateDE(){
+        colorState(DE, de);
+    }
+
+    @FXML void clickStateDE(Event event){
+        DE.setFill(Color.GREEN);
+        showPieChart(de);
+    }
+    @FXML void hoverOverStateDE(Event event){
+        DE.setFill(Color.LIGHTGREEN);
+    }
+    @FXML void exitOverStateDE(Event event){
+        colorStateDE();
+    }
 
     ///////////DISTRICT OF COLUMBIA///////////
 
@@ -182,9 +232,39 @@ public class FMXLDocumentController implements Initializable {
 
     State fl = App.GAME.USA.get(9);
 
+    @FXML void colorStateFL(){
+        colorState(FL, fl);
+    }
+
+    @FXML void clickStateFL(Event event){
+        FL.setFill(Color.GREEN);
+        showPieChart(fl);
+    }
+    @FXML void hoverOverStateFL(Event event){
+        FL.setFill(Color.LIGHTGREEN);
+    }
+    @FXML void exitOverStateFL(Event event){
+        colorStateFL();
+    }
+
     ///////////GEORGIA///////////
 
     State ga = App.GAME.USA.get(10);
+
+    @FXML void colorStateGA(){
+        colorState(GA, ga);
+    }
+
+    @FXML void clickStateGA(Event event){
+        GA.setFill(Color.GREEN);
+        showPieChart(ga);
+    }
+    @FXML void hoverOverStateGA(Event event){
+        GA.setFill(Color.LIGHTGREEN);
+    }
+    @FXML void exitOverStateGA(Event event){
+        colorStateGA();
+    }
 
     ///////////HAWAII///////////
 
@@ -325,14 +405,82 @@ public class FMXLDocumentController implements Initializable {
 
     ///////////MEINE///////////
 
+    State me = App.GAME.USA.get(19);
+
+    @FXML void colorStateME(){
+        colorState(ME, me);
+    }
+
+    @FXML void clickStateME(Event event){
+        ME.setFill(Color.GREEN);
+        showPieChart(me);
+    }
+    @FXML void hoverOverStateME(Event event){
+        ME.setFill(Color.LIGHTGREEN);
+    }
+    @FXML void exitOverStateME(Event event){
+        colorStateME();
+    }
+
 
     ///////////MARYLAND///////////
 
+    State md = App.GAME.USA.get(20);
+
+    @FXML void colorStateMD(){
+        colorState(MD, md);
+    }
+
+    @FXML void clickStateMD(Event event){
+        MD.setFill(Color.GREEN);
+        showPieChart(md);
+    }
+    @FXML void hoverOverStateMD(Event event){
+        MD.setFill(Color.LIGHTGREEN);
+    }
+    @FXML void exitOverStateMD(Event event){
+        colorStateMD();
+    }
+
     ///////////MASSACHUSETTS///////////
 
-    ///////////LUISIANNA///////////
+    State ma = App.GAME.USA.get(21);
+
+    @FXML void colorStateMA(){
+        colorState(MA, ma);
+    }
+
+    @FXML void clickStateMA(Event event){
+        MA.setFill(Color.GREEN);
+        showPieChart(ma);
+    }
+    @FXML void hoverOverStateMA(Event event){
+        MA.setFill(Color.LIGHTGREEN);
+    }
+    @FXML void exitOverStateMA(Event event){
+        colorStateMA();
+    }
+
 
     ///////////MICHIGAN///////////
+
+    State mi = App.GAME.USA.get(22);
+
+    @FXML void colorStateMI(){
+        colorState(MI, mi);
+    }
+
+    @FXML void clickStateMI(Event event){
+        MI.setFill(Color.GREEN);
+        showPieChart(mi);
+    }
+    @FXML void hoverOverStateMI(Event event){
+        MI.setFill(Color.LIGHTGREEN);
+    }
+    @FXML void exitOverStateMI(Event event){
+        colorStateMI();
+    }
+
 
     ///////////MINNESOTA///////////
 
@@ -451,9 +599,41 @@ public class FMXLDocumentController implements Initializable {
 
     ///////////NEW HAMPSHIRE///////////
 
-    ///////////NEW HAMPSHIRE///////////
+    State nh = App.GAME.USA.get(29);
+
+    @FXML void colorStateNH(){
+        colorState(NH, nh);
+    }
+
+    @FXML void clickStateNH(Event event){
+        NH.setFill(Color.GREEN);
+        showPieChart(nh);
+    }
+    @FXML void hoverOverStateNH(Event event){
+        NH.setFill(Color.LIGHTGREEN);
+    }
+    @FXML void exitOverStateNH(Event event){
+        colorStateNH();
+    } 
 
     ///////////NEW JARSEY///////////
+
+    State nj = App.GAME.USA.get(30);
+
+    @FXML void colorStateNJ(){
+        colorState(NJ, nj);
+    }
+
+    @FXML void clickStateNJ(Event event){
+        NJ.setFill(Color.GREEN);
+        showPieChart(nj);
+    }
+    @FXML void hoverOverStateNJ(Event event){
+        NJ.setFill(Color.LIGHTGREEN);
+    }
+    @FXML void exitOverStateNJ(Event event){
+        colorStateNJ();
+    } 
 
     ///////////NEW MEXICO///////////
 
@@ -476,7 +656,41 @@ public class FMXLDocumentController implements Initializable {
 
     ///////////NEW YORK///////////
 
+    State ny = App.GAME.USA.get(32);
+
+    @FXML void colorStateNY(){
+        colorState(NY, ny);
+    }
+
+    @FXML void clickStateNY(Event event){
+        NY.setFill(Color.GREEN);
+        showPieChart(ny);
+    }
+    @FXML void hoverOverStateNY(Event event){
+        NY.setFill(Color.LIGHTGREEN);
+    }
+    @FXML void exitOverStateNY(Event event){
+        colorStateNY();
+    }
+
     ///////////NORTH CAROLINA///////////
+
+    State nc = App.GAME.USA.get(33);
+
+    @FXML void colorStateNC(){
+        colorState(NC, nc);
+    }
+
+    @FXML void clickStateNC(Event event){
+        NC.setFill(Color.GREEN);
+        showPieChart(nc);
+    }
+    @FXML void hoverOverStateNC(Event event){
+        NC.setFill(Color.LIGHTGREEN);
+    }
+    @FXML void exitOverStateNC(Event event){
+        colorStateNC();
+    }
 
     ///////////NORTH DAKOTA///////////
 
@@ -498,6 +712,24 @@ public class FMXLDocumentController implements Initializable {
     }
 
     ///////////OHIO///////////
+    
+    State oh = App.GAME.USA.get(35);
+
+    @FXML void colorStateOH(){
+        colorState(OH, oh);
+    }
+
+    @FXML void clickStateOH(Event event){
+        showPieChart(oh);
+        OH.setFill(Color.GREEN);
+    }
+    @FXML void hoverOverStateOH(Event event){
+        OH.setFill(Color.LIGHTGREEN);
+    }
+    @FXML void exitOverStateOH(Event event){
+        colorStateOH();
+    }
+    
 
     ///////////OKLAHOMA///////////
     State ok = App.GAME.USA.get(36);
@@ -538,9 +770,60 @@ public class FMXLDocumentController implements Initializable {
 
     ///////////PENNSYLVANIA///////////
 
+    State pa = App.GAME.USA.get(38);
+
+    @FXML void colorStatePA(){
+        colorState(PA, pa);
+    }
+
+    @FXML void clickStatePA(Event event){
+        showPieChart(pa);
+        PA.setFill(Color.GREEN);
+    }
+    @FXML void hoverOverStatePA(Event event){
+        PA.setFill(Color.LIGHTGREEN);
+    }
+    @FXML void exitOverStatePA(Event event){
+        colorStatePA();
+    }
+
     ///////////RHODE ISLAND///////////
 
+    State ri = App.GAME.USA.get(39);
+
+    @FXML void colorStateRI(){
+        colorState(RI, ri);
+    }
+
+    @FXML void clickStateRI(Event event){
+        showPieChart(ri);
+        RI.setFill(Color.GREEN);
+    }
+    @FXML void hoverOverStateRI(Event event){
+        RI.setFill(Color.LIGHTGREEN);
+    }
+    @FXML void exitOverStateRI(Event event){
+        colorStateRI();
+    }
+
     ///////////SOUTH CAROLINA///////////
+
+    State sc = App.GAME.USA.get(40);
+
+    @FXML void colorStateSC(){
+        colorState(SC, sc);
+    }
+
+    @FXML void clickStateSC(Event event){
+        showPieChart(sc);
+        SC.setFill(Color.GREEN);
+    }
+    @FXML void hoverOverStateSC(Event event){
+        SC.setFill(Color.LIGHTGREEN);
+    }
+    @FXML void exitOverStateSC(Event event){
+        colorStateSC();
+    }
 
     ///////////SOUTH DAKOTA///////////
 
@@ -620,7 +903,41 @@ public class FMXLDocumentController implements Initializable {
 
     ///////////VERMONT///////////
 
+    State vt = App.GAME.USA.get(45);
+
+    @FXML void colorStateVT(){
+        colorState(VT, vt);
+    }
+
+    @FXML void clickStateVT(Event event){
+        VT.setFill(Color.GREEN);
+        showPieChart(vt);
+    }
+    @FXML void hoverOverStateVT(Event event){
+        VT.setFill(Color.LIGHTGREEN);
+    }
+    @FXML void exitOverStateVT(Event event){
+        colorStateVT();
+    }
+
     ///////////VIRGINIA///////////
+
+    State va = App.GAME.USA.get(46);
+
+    @FXML void colorStateVA(){
+        colorState(VA, va);
+    }
+
+    @FXML void clickStateVA(Event event){
+        VA.setFill(Color.GREEN);
+        showPieChart(va);
+    }
+    @FXML void hoverOverStateVA(Event event){
+        VA.setFill(Color.LIGHTGREEN);
+    }
+    @FXML void exitOverStateVA(Event event){
+        colorStateVA();
+    }
 
     ///////////WASCHINGTON///////////
 
@@ -643,7 +960,40 @@ public class FMXLDocumentController implements Initializable {
 
     ///////////WEST VIRGINIA///////////
 
+    State wv = App.GAME.USA.get(48);
+
+    @FXML void colorStateWV(){
+        colorState(WV, wv);
+    }
+
+    @FXML void clickStateWV(Event event){
+        WV.setFill(Color.GREEN);
+        showPieChart(wv);
+    }
+    @FXML void hoverOverStateWV(Event event){
+        WV.setFill(Color.LIGHTGREEN);
+    }
+    @FXML void exitOverStateWV(Event event){
+        colorStateWV();
+    }
+
     ///////////WINSOCSIN///////////
+    State wi = App.GAME.USA.get(49);
+
+    @FXML void colorStateWI(){
+        colorState(WI, wi);
+    }
+
+    @FXML void clickStateWI(Event event){
+        WI.setFill(Color.GREEN);
+        showPieChart(wi);
+    }
+    @FXML void hoverOverStateWI(Event event){
+        WI.setFill(Color.LIGHTGREEN);
+    }
+    @FXML void exitOverStateWI(Event event){
+        colorStateWI();
+    }
 
 
     ///////////WYOMING///////////
@@ -708,6 +1058,10 @@ public class FMXLDocumentController implements Initializable {
         colorStateAZ();
         colorStateCA();
         colorStateCO();
+        colorStateCT();
+        colorStateDE();
+        colorStateFL();
+        colorStateGA();
         colorStateID();
         colorStateIL();
         colorStateIN();
@@ -715,21 +1069,37 @@ public class FMXLDocumentController implements Initializable {
         colorStateKS();
         colorStateKY();
         colorStateLA();
+        colorStateMA();
+        colorStateME();
+        colorStateMD();
+        colorStateMI();
         colorStateMN();
         colorStateMS();
         colorStateMO();
         colorStateMT();
         colorStateNV();
+        colorStateNY();
         colorStateNE();
+        colorStateNJ();
+        colorStateNH();
         colorStateNM();
+        colorStateNC();
         colorStateND();
+        colorStateOH();
         colorStateOR();
         colorStateOK();
+        colorStatePA();
+        colorStateRI();
+        colorStateSC();
         colorStateSD();
         colorStateTN();
         colorStateTX();
         colorStateUT();
+        colorStateVA();
+        colorStateVT();
         colorStateWA();
+        colorStateWI();
+        colorStateWV();
         colorStateWY();
     }
 
